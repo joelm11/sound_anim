c1894cb9c3c9f047ebd69f8d03129a3263bc18e3
Joel Meuleman <j-jolabola@live.com>
2025-05-09 16:36:41 -0600

    initial epoch loudness calculator.

--- src/speaker_points/speaker_dbs.hpp
+++ src/speaker_points/speaker_dbs.hpp
@@ -1,3 +1,4 @@
+#include <AudioFile.h>
 #include <vector>
 
 struct LoudnessEpoch {
@@ -7,15 +8,40 @@ struct LoudnessEpoch {
 
 class LoudnessGenerator {
 public:
-  LoudnessGenerator(const std::string inFile)
-      : kInFile(inFile), kInSampleRate() {
+  LoudnessGenerator(const std::string inPath, const float epochLength_s)
+      : kInputPath(inPath), kEpochLength_s(epochLength_s) {
+
     // Load input audio file here and record its sample rate and stuff.
-    kInSampleRate = 0;
+    inFile.load(kInputPath);
+    sampleRate = inFile.getSampleRate();
+    length_s = inFile.getLengthInSeconds();
+    samplesPerEpoch = kEpochLength_s / sampleRate;
   }
 
-  LoudnessEpoch nextLoudnessEpoch() { return {}; };
+  float getLength_s() const { return length_s; }
+
+  LoudnessEpoch nextLoudnessEpoch() {
+    if (sampleIdx + samplesPerEpoch > inFile.getNumSamplesPerChannel()) {
+      return {-1, {}};
+    }
+    std::vector<float> ldness(inFile.getNumChannels());
+    for (int ch = 0; ch < inFile.getNumChannels(); ++ch) {
+      float rms = 0;
+      for (int sample = 0; sample < samplesPerEpoch; ++sample) {
+        rms += inFile.samples[ch][float] * inFile.samples[ch][float];
+      }
+      rms /= samplesPerEpoch;
+      ldness[ch] = std::sqrt(rms);
+    }
+    sampleIdx += samplesPerEpoch;
+    const float kTimeStamp = sampleIdx / sampleRate;
+    return {1.f, ldness};
+  };
 
 private:
-  const std::string kInFile;
-  float kInSampleRate;
+  const std::string kInputPath;
+  const float kEpochLength_s;
+  float sampleRate, length_s, samplesPerEpoch;
+  unsigned sampleIdx = 0;
+  AudioFile<float> inFile;
 };
